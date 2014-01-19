ba044c6de391dfa74aa28d56f37d1bdf44c71ab1
Ellen Chisa <ellen.chisa@gmail.com>
2014-01-19 12:49:22 -0500

    Adding fb log in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,22 @@ class HomeController < ApplicationController
         
     end 
 
+    def oauth
+        oauth = Koala::Facebook::OAuth.new(oauth_url)
+
+        access_token = oauth.get_access_token(params[:code])
+
+        graph= Koala::Facebook::API.new(access_token)
+        fb_user= graph.get_object('me')
+
+        session[:fbuser] = fb_user
+
+        redirect_to root_url
+    end
+
+    def logout
+        session[:fbuser] = nil
+        redirect_to root_url
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Energy::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  get 'oauth' => 'home#oauth'
+  get 'logout' => 'home#logout'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
