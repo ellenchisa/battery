b841c4c602c966dc92c038036d82a95bfa6e435c
Ellen Chisa <ellen.chisa@gmail.com>
2014-01-19 12:21:26 -0500

    Gets most recent update, adds fixture data
    Created Fixture Data
    Made battery to only most recent update

--- app/models/battery.rb
+++ app/models/battery.rb
@@ -1,2 +1,3 @@
 class Battery < ActiveRecord::Base
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+    has_many :batteries
+    has_one :battery, order: 'created_at DESC'
 end
