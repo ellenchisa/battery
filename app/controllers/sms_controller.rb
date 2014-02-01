babc5796caed0a93b4d613bb3fca2474e0f86185
Ellen Chisa <ellen.chisa@gmail.com>
2014-02-01 16:35:00 -0500

    validates request comes from twilio

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -4,27 +4,29 @@ protect_from_forgery :except => [:index]
 
     def index
 
-        message_body = params["Body"].downcase
-        from_number = params["From"].downcase
+        if params["AccountSid"] == Twilio::SID
 
-        puts message_body,from_number
+            message_body = params["Body"].downcase
+            from_number = params["From"].downcase
 
-        user = User.find_by_phone(from_number)
+            user = User.find_by_phone(from_number)
 
-        if user
-            battery = Battery.new
-            if message_body == 'better'
-                battery.level = user.battery.level + 1
-            elsif message_body == 'worse'
-                battery.level = user.battery.level - 1
-            elsif message_body == 'same'
-                battery.level = user.battery.level
+            if user
+                battery = Battery.new
+                if message_body == 'better'
+                    battery.level = user.battery.level + 1
+                elsif message_body == 'worse'
+                    battery.level = user.battery.level - 1
+                elsif message_body == 'same'
+                    battery.level = user.battery.level
+                end
+                user.batteries << battery
+                user.save!
             end
-            user.batteries << battery
-            user.save!
         end
 
         render :text => 'okay!'
+        
     end
 
 end
