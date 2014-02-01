af21772386992c249731b9357d7be4a112d4da44
Ellen Chisa <ellen.chisa@gmail.com>
2014-02-01 16:27:50 -0500

    validating level (no loops!)

--- app/models/battery.rb
+++ app/models/battery.rb
@@ -1,4 +1,16 @@
 class Battery < ActiveRecord::Base
     belongs_to :user
     validates_presence_of :user, :level
+    validates_inclusion_of :level, :in => 1..4
+
+    before_validation :set_levels
+
+    private
+        def set_levels
+            if self.level > 4
+                self.level = 4
+            elsif self.level < 1
+                self.level = 1
+            end
+        end
 end
